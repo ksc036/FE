e5919ba87b73aefe75b6c7ad751e0b35a2305839
ksc <ksc036@gmail.com>
2023-07-26 15:42:52 +0900

    socket뺴고 테스트

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,101 +1,98 @@
 <template>
-
   <h1>Room</h1>
   <div id="welcome" v-if="!chat">
-    <input type="text" placeholder="room name" required v-model="roomName">
+    <input type="text" placeholder="room name" required v-model="roomName" />
     <button @click="enter_room">Enter Room</button>
   </div>
 
   <div v-if="chat">
-    <h1>{{roomName}}</h1>
+    <h1>{{ roomName }}</h1>
     <div>
       <video ref="video" autoplay width="400" height="400"></video>
-      <button id="mic" @click="cameraClick">{{ camera? "카메라 끄기" : "카메라 켜기"}}</button>
-      <button id="mute" @click="muteClick">{{ mic ? "마이크 끄기" : "마이크 켜기"}}</button>
+      <button id="mic" @click="cameraClick">
+        {{ camera ? "카메라 끄기" : "카메라 켜기" }}
+      </button>
+      <button id="mute" @click="muteClick">
+        {{ mic ? "마이크 끄기" : "마이크 켜기" }}
+      </button>
 
       <div class="peerStream">
-      <video ref="peer" autoplay width="400" height="400"></video>
+        <video ref="peer" autoplay width="400" height="400"></video>
+      </div>
     </div>
-    </div>
-
-
   </div>
-
 </template>
 
 <script>
-
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
 
-  created(){
+  created() {
     // this.getMedia();
-    this.$socket.on("welcome", async () =>{
-      const offer = await this.myPeerConnection.createOffer();
-      this.myPeerConnection.setLocalDescription(offer);
-      console.log("sent offer");
-
-      this.$socket.emit("offer", offer, this.roomName);
-      // console.log(offer);
-    })
-
-    this.$socket.on("offer", async (offer) =>{
-      console.log("receive offer");
-      this.myPeerConnection.setRemoteDescription(offer);
-      const answer = await this.myPeerConnection.createAnswer();
-      // console.log(answer);
-
-      this.myPeerConnection.setLocalDescription(answer);
-      this.$socket.emit("answer",answer,this.roomName);
-      console.log("sent answer");
-    })
-
-    this.$socket.on("answer",(answer) =>{
-      console.log("receive the answer");
-      this.myPeerConnection.setRemoteDescription(answer);
-    })
-
-    this.$socket.on("ice",(ice) =>{
-      console.log("recive candidate");
-      this.myPeerConnection.addIceCandidate(ice);
-    })
+    // this.$socket.on("welcome", async () =>{
+    //   const offer = await this.myPeerConnection.createOffer();
+    //   this.myPeerConnection.setLocalDescription(offer);
+    //   console.log("sent offer");
+    //   this.$socket.emit("offer", offer, this.roomName);
+    //   // console.log(offer);
+    // })
+    // this.$socket.on("offer", async (offer) =>{
+    //   console.log("receive offer");
+    //   this.myPeerConnection.setRemoteDescription(offer);
+    //   const answer = await this.myPeerConnection.createAnswer();
+    //   // console.log(answer);
+    //   this.myPeerConnection.setLocalDescription(answer);
+    //   this.$socket.emit("answer",answer,this.roomName);
+    //   console.log("sent answer");
+    // })
+    // this.$socket.on("answer",(answer) =>{
+    //   console.log("receive the answer");
+    //   this.myPeerConnection.setRemoteDescription(answer);
+    // })
+    // this.$socket.on("ice",(ice) =>{
+    //   console.log("recive candidate");
+    //   this.myPeerConnection.addIceCandidate(ice);
+    // })
   },
-  data : () => {
+  data: () => {
     return {
-      myStream : {},
-      mic : true,
-      camera : true,
-      chat : false,
-      roomName : "",
-    }
+      myStream: {},
+      mic: true,
+      camera: true,
+      chat: false,
+      roomName: "",
+    };
   },
   props: {
     msg: String,
   },
-  methods : {
-    async getMedia(){
-      try{
+  methods: {
+    async getMedia() {
+      try {
         this.myStream = await navigator.mediaDevices.getUserMedia({
-          audio : true,
-          video :true
-        })
+          audio: true,
+          video: true,
+        });
         // console.log(this.myStream);
         this.$refs.video.srcObject = this.myStream;
         // await this.getCamera();
         // this.$refs.video.play();
         // console.log(this.$refs.video)
-
-      }catch(e){
+      } catch (e) {
         console.log(e);
       }
     },
-    cameraClick(){
-      this.myStream.getVideoTracks().forEach((track) => (track.enabled = ! track.enabled));
+    cameraClick() {
+      this.myStream
+        .getVideoTracks()
+        .forEach((track) => (track.enabled = !track.enabled));
       // console.log(this.myStream.getVideoTracks());
       this.camera = !this.camera;
     },
-    muteClick(){
-      this.myStream.getAudioTracks().forEach((track) => (track.enabled = ! track.enabled));
+    muteClick() {
+      this.myStream
+        .getAudioTracks()
+        .forEach((track) => (track.enabled = !track.enabled));
       // console.log(this.myStream.getAudioTracks());
       this.mic = !this.mic;
     },
@@ -109,41 +106,39 @@ export default {
     //     console.log(e);
     //   }
     // },
-    async enter_room(){
+    async enter_room() {
       await this.initCall();
-      this.$socket.emit("join_room",this.roomName);
+      this.$socket.emit("join_room", this.roomName);
     },
-    async initCall(){
+    async initCall() {
       this.chat = !this.chat;
       await this.getMedia();
       this.makeConnection(); //RTC객체를 생성하고 컴포넌트 전역에 선언함.
     },
-    makeConnection(){
+    makeConnection() {
       const peerConnection = new RTCPeerConnection();
       this.myPeerConnection = peerConnection;
       this.myPeerConnection.addEventListener("icecandidate", this.handleIce);
-      this.myPeerConnection.addEventListener("addstream", this.handleAddStream)
+      this.myPeerConnection.addEventListener("addstream", this.handleAddStream);
       // console.log(peerConnection);
-      this.myStream.getTracks().forEach(track =>{
-        this.myPeerConnection.addTrack(track,this.myStream);
+      this.myStream.getTracks().forEach((track) => {
+        this.myPeerConnection.addTrack(track, this.myStream);
       });
     },
-    handleIce(data){
+    handleIce(data) {
       console.log("send candidate");
       this.$socket.emit("ice", data.candidate, this.roomName);
       // console.log(data);
     },
-    handleAddStream(data){
+    handleAddStream(data) {
       console.log("got my peer");
       // console.log(data); //data.stream 이 동료영상
       this.$refs.peer.srcObject = data.stream;
       console.log(data.stream);
-    }
-},
-}
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import io from 'socket.io-client'
+// import io from 'socket.io-client'
 
-const socket = io("wss://i9a701.p.ssafy.io");
+// const socket = io("wss://i9a701.p.ssafy.io");
 // const socket = io("http://localhost:4060");//test
 
 
 
 // App.config.globalProperties.$socket = socket;
 const app = createApp(App);
-app.config.globalProperties.$socket = socket;
-console.log(socket);
+// app.config.globalProperties.$socket = socket;
+// console.log(socket);
+
 
 app.mount('#app')
 // console.log(app.config.globalProperties.$socket);
 // console.log(App);
 // createApp(App).mount('#app')
-//팀원간 테스트까지 마친 최종코드 이거돌리고 프런트는 다운받아서 돌리니깐 잘돌아감
+//팀원간 테스트까지 마친 최종코드 이거돌리고 프런트
