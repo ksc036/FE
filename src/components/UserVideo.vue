d541cb2b4b18e508d45f44c42caba2df7f151f51
ksc <ksc036@gmail.com>
2023-07-28 08:41:56 +0900

    log 제거

--- src/components/UserVideo.vue
+++ src/components/UserVideo.vue
@@ -1,32 +1,30 @@
 <template>
-        <video ref="remote" autoplay width="400" height="400"></video>
-        <span>{{ info.nickname }}</span>
-  </template>
-  <script>
-  export default {
-    name: "UserVideo",
-    components: {
-    },created(){
-
-    },mounted(){
-        console.log(this.info);
-        console.log(this.$refs);
-        this.$refs.remote.srcObject = this.info.stream;
-    },
-    props: {
+  <video ref="remote" autoplay width="400" height="400"></video>
+  <span>{{ info.nickname }}</span>
+</template>
+<script>
+export default {
+  name: "UserVideo",
+  components: {},
+  created() {},
+  mounted() {
+    // console.log(this.info);
+    // console.log(this.$refs);
+    this.$refs.remote.srcObject = this.info.stream;
+  },
+  props: {
     info: Object,
   },
-  };
-  </script>
-  
-  <style>
-  #app {
-    /* font-family: Avenir, Helvetica, Arial, sans-serif; */
-    /* -sebkit-font-smoothing: antialiased;
+};
+</script>
+
+<style>
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  /* -sebkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px; */
-  }
-  </style>
-  
+}
+</style>
