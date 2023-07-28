e9b785ab7d67fdfafb6afd634dfb1adb0b5cde48
ksc <ksc036@gmail.com>
2023-07-28 11:08:53 +0900

    최종 : 서버는 masterbranch 프런트는 localbranch로 배포

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import io from 'socket.io-client'
 
-const socket = io("http://localhost:9000");
+const socket = io("wss://i9a701.p.ssafy.io");
 
 
 // App.config.globalProperties.$socket = socket;
