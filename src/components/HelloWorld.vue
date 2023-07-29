843f6323869d49519147269e39baaacf8740c29e
ksc <ksc036@gmail.com>
2023-07-30 05:34:30 +0900

    stun 제거

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -110,21 +110,6 @@ export default {
           iceServers: [
         {
             urls : [
-              "stun:stun.ekiga.net",
-              "stun:ideasip.com",
-              "stun:stun.rixtelecom.se",
-              "stun:stun.schlund.de",
-"stun:stun.stunprotocol.org:3478",
-"stun:stun.voiparound.com",
-"stun:stun.voipbuster.com",
-"stun:stun.voipstunt.com",
-"stun:stun.voxgratia.org",
-              "stun:stun.l.google.com:19302",
-              "stun:stun1.l.google.com:19302",
-              "stun:stun2.l.google.com:19302",
-              "stun:stun3.l.google.com:19302",
-              "stun:stun4.l.google.com:19302",
-              "iphone-stun.strato-iphone.de:3478"
           ],
         },
         {
